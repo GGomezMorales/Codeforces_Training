86ae9e56d3511ea2e729cbee1fe4a64d7518c36a
GGomezMorales <gugomog3@gmail.com>
2023-05-08 23:08:09 -0500

    training

---
+++ drinks.py
@@ -0,0 +1,3 @@
+n = int(input())
+percent_drinks = list(map(int, input().split(" ")))
+print((sum(percent_drinks)/n))
---
+++ presents.py
@@ -0,0 +1,7 @@
+n = int(input())
+gifts = list(map(int, input().split()))
+givers = [0] * n
+for i in range(n):
+    givers[gifts[i] - 1] = i + 1
+
+print(*givers)

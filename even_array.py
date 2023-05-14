07c4e0fb3978914a67188962ec9f4f7607158263
GGomezMorales <gugomog3@gmail.com>
2023-05-14 00:35:29 -0500

    training

--- even_array.py
+++ even_array.py
@@ -5,16 +5,16 @@ while test < number_test:
     test += 1
     n = int(input())
     array = [i % 2 for i in list(map(int, input().split(" ")))]
-    partern_array = [i % 2 for i in range(n)]
-    if (array.count(0) != partern_array.count(0)) or (array.count(0) != partern_array.count(0)):
+    pattern_array = [i % 2 for i in range(n)]
+    if (array.count(0) != pattern_array.count(0)) or (array.count(0) != pattern_array.count(0)):
         print(-1)
-    elif array == partern_array:
+    elif array == pattern_array:
         print(0)
     else:
         for j in range(n):
-            if array[j] != partern_array[j]:
+            if array[j] != pattern_array[j]:
                 count += 1
         print(count // 2)
     array.clear()
-    partern_array.clear()
+    pattern_array.clear()
     count = 0
